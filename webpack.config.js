40029853a64c1ab9368d1b85a67c73fc9ba87a4a
leo.yy <bluelight598>
2016-05-11 00:46:15 +0800

    feat:使用全局node_module

--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,19 @@ var webpackDevMiddleware = require('webpack-dev-middleware');
 var currentBase = __dirname; //process.cwd();
 var entryDir = 'src';
 var outputDir = 'build';
-var context = path.isAbsolute(entryDir) ? entryDir : path.join(process.cwd(), entryDir);
+var context = path.join(process.cwd(), entryDir); // 必须是绝对路径
 module.exports = {
 	// 定义全集load 目录; 吧node_modules目录加入到require中;
-	root: [context, currentBase + '/node_modules'],
+	// root: [context, path.join(currentBase, './node_modules')],
 	entry: {
 		'p/index/index': './src/p/index/index.js',
 	},
 	output: {
 		path: path.resolve(currentBase, 'build'),
 		filename: '[name].js'
+	},
+	resolveLoader: {
+		root: path.join(__dirname, "./node_modules")
 	},
 	externals: { // 使用CDN/远程文件
 		// zepto: 'zepto',
@@ -29,11 +32,12 @@ module.exports = {
 				loader: 'babel-loader!jsx-loader?harmony'
 			}, {
 				test: /\.js$/,
-				loaders: ['babel-loader'],
-				exclude: path.join(currentBase,'./node_modules')
+				loaders: ['babel-loader']
+					// exclude: path.join(currentBase, './node_modules')
 			}, {
 				test: /\.ejs$/,
-				loader: "ejs-loader?variable=data"
+				loader: "ejs-loader?variable=data",
+				exclude: path.join(currentBase, './node_modules')
 			}, {
 				test: /\.less$/,
 				loader: "style!css!less-loader"
@@ -46,9 +50,9 @@ module.exports = {
 			}
 		]
 	},
-
 	resolve: {
 		//require时候自动补全扩展名;
+		root: [path.join(currentBase, './node_modules'), path.join(process.cwd(), "./node_modules")],
 		extensions: ['', '.js', '.json', '.html'],
 		alias: { // 别名
 			zepto: 'webpack-zepto'
@@ -60,8 +64,12 @@ module.exports = {
 		//提供全局的变量，在模块中使用无需用require引入
 
 		new Webpack.ProvidePlugin({ // 如果不自动注入zepto，则需要用户在入口js手动写入 var $ = require('zepto');
-            $: "webpack-zepto"
-        }),
+			$: "webpack-zepto"
+		}),
+
+		new Webpack.ProvidePlugin({
+			_: "underscore"
+		}),
 
 		new Webpack.NoErrorsPlugin()
 	]
